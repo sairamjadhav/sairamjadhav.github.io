5a8eadad8ea2fdc457506312c5301448cc2b0218
sairamjadhav <sairamjadhav12@gmail.com>
2025-06-08 17:05:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Initialize AOS
 AOS.init({ duration: 1000, once: true });
 
+// Typewriter effect for About section
+const typewriter = new Typed('#typewriter', {
+  strings: [
+    "I like to code.py",
+    "I compile emotions.poetry",
+    "I run innings.cricket"
+  ],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 1500,
+  loop: true,
+  showCursor: true,
+  cursorChar: "|"
+});
+
 // Flip skill cards on click
 document.querySelectorAll('.skill-card').forEach(card => {
   card.addEventListener('click', () => {
